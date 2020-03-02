552acba9393f67026c6831e544ad732a832dac1a
sktanwar2014 <sktanwar.2014@gmail.com>
2020-03-02 16:39:49 +0530

    first time

---
+++ routes/mainRoute.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const mainRoute = require('express').Router();
+// const { env } = require("../lib/databaseMySQL");
+let env = 'prod';
+
+mainRoute.get('*', (req, res) => {
+  
+  // const route = path.join(__dirname, '..', 'dist', 'index.html');
+  
+  if (env === 'dev' || env === 'uat' || env === 'prod') {
+    const route = path.join(__dirname, '..', 'dist', 'index.html');
+    res.sendFile(route);
+  } else {
+    const route = path.join(__dirname, '..', '..', 'src', 'index.html');
+    res.sendFile(route);
+  }
+});
+
+module.exports = mainRoute;
