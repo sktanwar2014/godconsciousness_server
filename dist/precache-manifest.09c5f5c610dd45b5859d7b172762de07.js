e7ed0f81a52b2656abe5129520a4bc697c7d5574
sktanwar2014 <sktanwar.2014@gmail.com>
2020-03-02 16:47:46 +0530

    chage

---
+++ dist/precache-manifest.09c5f5c610dd45b5859d7b172762de07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37ff75e01c338514f6f91b5035b9479",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88912eaf3246244ded23",
+    "url": "/static/css/main.d7573f2d.chunk.css"
+  },
+  {
+    "revision": "5c2d0b5fa64ed7f57169",
+    "url": "/static/js/2.b0e7832c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.b0e7832c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88912eaf3246244ded23",
+    "url": "/static/js/main.837bbd19.chunk.js"
+  },
+  {
+    "revision": "d3e239ce1b6b40a7c935",
+    "url": "/static/js/runtime-main.0d4b79d0.js"
+  }
+]);
