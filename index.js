20bc6089fba21de068fbd04bc67824718d49e7fa
sktanwar2014 <sktanwar.2014@gmail.com>
2020-07-18 19:19:33 +0530

    dsf

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const path = require('path');
 
 const app = express();
 
-app.use(cors());
+app.use(cors());  
+// app.use(cors());  
 app.use(bodyParser.json());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
